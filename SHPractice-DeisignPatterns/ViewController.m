a8b36f9e0787acb2d6de5c0da6a9de111922ceb3
wangxiang <wangxiang@yixia.com>
2019-09-09 18:42:37 +0800

    Initial Commit

---
+++ SHPractice-DeisignPatterns/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  SHPractice-DeisignPatterns
+//
+//  Created by Shine on 2019/9/9.
+//  Copyright © 2019 shine. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
